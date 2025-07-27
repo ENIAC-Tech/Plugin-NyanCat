bbfad545ab8278db1af27ac8551113e4657f7360
aspen <icyqwq@gmail.com>
2025-07-27 23:43:19 +0800

    first commit

---
+++ src/plugin.js
@@ -0,0 +1,80 @@
+const { plugin, logger, pluginPath, resourcesPath } = require("@eniac/flexdesigner")
+
+// Store key data
+const keyData = {}
+const fs = require('fs')
+const path = require('path')
+let nyancatInterval = null
+const nyancats = []
+function loadPngImages() {
+    try {
+        const files = fs.readdirSync(resourcesPath)
+        files.forEach(file => {
+            if (path.extname(file).toLowerCase() === '.png') {
+                const filePath = path.join(resourcesPath, file)
+                const imageBuffer = fs.readFileSync(filePath)
+                const base64Data = imageBuffer.toString('base64')
+                const fileName = path.basename(file, '.png')
+                nyancats.push(`data:image/png;base64,${base64Data}`)
+            }
+        })
+    } catch (error) {
+        logger.error('Failed to load nyancats:', error)
+    }
+}
+loadPngImages()
+
+/**
+ * Called when device status changes
+ * @param {object} devices device status data
+ * [
+ *  {
+ *    serialNumber: '',
+ *    deviceData: {
+ *       platform: '',
+ *       profileVersion: '',
+ *       firmwareVersion: '',
+ *       deviceName: '',
+ *       displayName: ''
+ *    }
+ *  }
+ * ]
+ */
+plugin.on('device.status', (devices) => {
+    logger.info('Device status changed:', devices)        
+})
+
+
+/**
+ * Called when a plugin key is loaded
+ * @param {Object} payload alive key data
+ * {
+ *  serialNumber: '',
+ *  keys: []
+ * }
+ */
+plugin.on('plugin.alive', (payload) => {
+    logger.info('Plugin alive:', payload)
+    for (let key of payload.keys) {
+      keyData[key.uid] = key
+      if (key.cid === 'com.eniac.nyancat.nyancat') {
+        let nyancatIndex = 0
+        nyancatInterval = setInterval(() => {
+          try {
+            plugin.directDraw(payload.serialNumber, key, nyancats[nyancatIndex])
+          } catch (error) {
+            logger.error('Error drawing nyancat:', error)
+            clearInterval(nyancatInterval)
+            nyancatInterval = null
+          }
+          nyancatIndex++
+          if (nyancatIndex >= nyancats.length) {
+            nyancatIndex = 0
+          }
+        }, 30)
+      }
+    }
+})
+
+// Connect to flexdesigner and start the plugin
+plugin.start()
